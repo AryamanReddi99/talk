2a05a75e2baaffa88a48abb79b0638056327d783
AryamanReddi99 <aryamanreddi@gmail.com>
2026-06-26 14:59:49 +0200

    changes

--- talk/experiments/mpe/mordatch/sweep_obs_pred.sh
+++ talk/experiments/mpe/mordatch/sweep_obs_pred.sh
@@ -1,8 +1,7 @@
-for obs_pred_coef in 0.0 0.1 0.5 1.0 2.0; do
+for obs_pred_coef in 0.0 0.1 0.5 1.0; do
   echo "=== obs_pred_coef=${obs_pred_coef} ==="
   python mappo_mordatch.py \
     "obs_pred_coef=${obs_pred_coef}" \
-    "sight_range=1.5" \
-    "talk_config=_1.5_sr_${obs_pred_coef}_obspred"
+    "talk_config=_${obs_pred_coef}_obspred"
 done
 echo "Sweep complete."
