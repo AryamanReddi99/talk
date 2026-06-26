552303f384f68c98e8aa8061b29b3cb259339e29
AryamanReddi99 <aryamanreddi@gmail.com>
2026-06-26 16:14:32 +0200

    changes

--- talk/experiments/mpe/mordatch/sweep_word_coef.sh
+++ talk/experiments/mpe/mordatch/sweep_word_coef.sh
@@ -1,8 +1,8 @@
-for word_coef in 0.0 0.001 0.01 0.05 0.1; do
+for word_coef in 0.0 0.001 0.01 0.05 0.1 0.5 1.0; do
   echo "=== word_coef=${word_coef} ==="
   python mappo_mordatch.py \
     "word_coef=${word_coef}" \
-    "sight_range=1.5" \
-    "talk_config=_1.5_sr_${word_coef}_word"
+    "sight_range=0.5" \
+    "talk_config=_${word_coef}_word"
 done
 echo "Sweep complete."
